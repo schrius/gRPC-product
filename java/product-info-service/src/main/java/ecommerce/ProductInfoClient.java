2e6d4863a70ecbf9b93745810fbfa523f45dc87b
Schrius <schriusty@gmail.com>
2020-06-10 17:19:13 -0400

    Java Client

---
+++ java/product-info-service/src/main/java/ecommerce/ProductInfoClient.java
@@ -0,0 +1,36 @@
+package ecommerce;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.logging.Logger;
+
+/**
+ * * gRPC client sample for productInfo service.
+*/
+public class ProductInfoClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        ManagedChannel channel = ManagedChannelBuilder
+            .forAddress("localhost", 50051) 
+            .usePlaintext()
+            .build();
+ 
+        ProductInfoGrpc.ProductInfoBlockingStub stub =
+               ProductInfoGrpc.newBlockingStub(channel); 
+ 
+        ProductInfoOuterClass.ProductID productID = stub.addProduct(    
+                ProductInfoOuterClass.Product.newBuilder()
+                        .setName("Apple iPhone 11")
+                        .setDescription("Meet Apple iPhone 11. " +
+                             All-new dual-camera system with " +
+                             "Ultra Wide and Night mode.");
+                        .setPrice(1000.0f)
+                        .build());
+        System.out.println(productID.getValue());
+ 
+        ProductInfoOuterClass.Product product = stub.getProduct(productID);  
+        System.out.println(product.toString());
+        channel.shutdown(); 
+    }
+ }
---
+++ java/product-info-service/src/main/java/product/info/service/App.java
@@ -0,0 +1,28 @@
+package ecommerce;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class ProductInfoServer {
+
+   public static void main(String[] args)
+           throws IOException, InterruptedException {
+       int port = 50051;
+       Server server = ServerBuilder.forPort(port) 
+               .addService(new ProductInfoImpl())
+               .build()
+               .start();
+       System.out.println("Server started, listening on " + port);
+       Runtime.getRuntime().addShutdownHook(new Thread(() -> { 
+           System.err.println("Shutting down gRPC server since JVM is " +
+                "shutting down");
+           if (server != null) {
+               server.shutdown();
+           }
+           System.err.println(“Server shut down");
+       }));
+       server.awaitTermination(); 
+   }
+}
